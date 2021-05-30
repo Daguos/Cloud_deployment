39e70d87998965fc37e22ebbd0ec49cba7777a7e
Daguos <69721056+Daguos@users.noreply.github.com>
2021-05-30 18:51:33 -0500

    Update 01-create-workspace.py

--- 01-create-workspace.py
+++ 01-create-workspace.py
@@ -1,10 +1,10 @@
 from azureml.core.authentication import InteractiveLoginAuthentication
 from azureml.core import Workspace
 
-interactive_auth = InteractiveLoginAuthentication(tenant_id='a1b3008e-5732-4f15-8292-cf478f19c818')
+interactive_auth = InteractiveLoginAuthentication(tenant_id='')
 ws = Workspace.create(
     name='azure-ml-demand',
-    subscription_id='d916feb2-5bfe-41e6-9c49-b315b78d95c0',
+    subscription_id='',
     resource_group='demand_machine_learning',
     create_resource_group=True,
     location='eastus2',
